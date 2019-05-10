55901c3f5bbd97c54aaa3c51261576e28be3dffe
Prateek76 <iec2017029@iiita.ac.in>
2019-05-11 02:10:54 +0530

    aterial added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 import './Nav.css'
 
 class Nav extends Component {
@@ -7,9 +10,20 @@ class Nav extends Component {
         return (
             <div className="Nav">
                 <ul className="Nav-Items">
-                    <li className="Items">First</li>
-                    <li className="Items">Second</li>
-                    <li className="Items">Third</li>
+                <div className="search-parent">
+                    <div className="search-icon">
+                        <SearchIcon />
+                    </div>
+                    <InputBase
+                    placeholder="Search…"
+                    />
+                </div>
+                    <li className="Items">
+                        <Button variant="outlined" size="large" className="login">Login</Button>
+                    </li>
+                    <li className="Items">
+                    <Button variant="contained" size="large" color="primary" className="sign-up">Large</Button>
+                    </li>
                 </ul>
             </div>
         );
