c4da74ea35148a7590ec9b77fd0be9f5e39070c7
Prateek76 <iec2017029@iiita.ac.in>
2019-05-11 17:24:16 +0530

    banner added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ImageBanner from './image.jpg';
 
 class Header extends Component {
     render() {
         return (
             <div className="header">
-                <img src="./image.jpg" />
+                <img style={{width: '100%'}} alt="banner" src={ImageBanner} />
             </div>
         );
     }
